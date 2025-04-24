a979447ece12105fc68be8df78fba32def510aee
TooMuchRakja <szymekp1994@gmail.com>
2025-04-24 17:12:13 +0200

    Dodano isDone do tabeli dynamo, przycisk delete, oraz autentyfikacje

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
+
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,22 +20,34 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>Moje zadania</h1>
+      <button onClick={createTodo}>+ Nowe zadanie</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+     <li key={todo.id} style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+     <span>{todo.content}</span>
+     <button onClick={() => deleteTodo(todo.id)}>🗑️</button>
+   </li>
+   
+      
+
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 Aplikacja dziala. Uzywaj do woli polaczku.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
+          Kliknij w ikone kosza aby usunac.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
